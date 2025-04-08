f05fe2a919358f3dc7dce2d06674378304f119d7
anubhavpurohit <anubhavpurohitno1@gmail.com>
2025-04-08 12:25:29 +0530

    main logic id done

--- apps/ws-backend/src/Game.ts
+++ apps/ws-backend/src/Game.ts
@@ -1,4 +1,5 @@
 import { WebSocket, WebSocketServer } from "ws"
+import { generateParagraph } from "./generateparagraph";
 
 interface Player{
 id:string,
@@ -21,6 +22,31 @@ export class Game{
         this.gameStatus = "not-started";
         this.paragraph = "";
     }
+    maingamelogic(ws:WebSocket,playerid:string){
+        ws.on("message",(data:string)=>{
+            const parseddata=JSON.parse(data)
+
+            if(parseddata.type=="start-game"){
+                if(this.gameStatus==="in-progress"){
+                    return 
+                }
+                if(this.gameHost!==playerid){
+                    return
+                }
+                this.players.forEach((player)=>player.score=0)
+                this.bodcast({type:"players",players:this.players})
+                this.paragraph=generateParagraph()
+                this.gameStatus="in-progress"
+                this.bodcast({type:"game-started",paragraph:this.paragraph})
+
+                setTimeout(() => {
+                    this.gameStatus="finished"
+                    this.bodcast({type:"game-finished"})
+                    this.bodcast({type:"players",players:this.players})
+                }, 60000);
+            }
+        })
+    }
     JoinGame(id:string,name:string,ws:WebSocket){
         if(this.gameStatus=="in-progress"){
             return 
@@ -29,6 +55,7 @@ export class Game{
         this.bodcast({type:"player-joined",id:id,name:name,score:0})
         ws.send(JSON.stringify({ type: "player", players: this.players }));
         ws.send(JSON.stringify({ type: "new-host", host: this.gameHost }));
+        this.maingamelogic(ws,id)
 
     }
     bodcast(data:any){
